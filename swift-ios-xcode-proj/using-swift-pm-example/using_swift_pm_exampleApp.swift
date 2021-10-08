e814844735c3752216d70892a53af4b91587a2fc
Megh <megh@fossa.com>
2021-10-08 13:11:33 -0600

    Adds example swift and php project

---
+++ swift-ios-xcode-proj/using-swift-pm-example/using_swift_pm_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  using_swift_pm_exampleApp.swift
+//  using-swift-pm-example
+//
+//  Created by Megh on 2021-10-08.
+//
+
+import SwiftUI
+
+@main
+struct using_swift_pm_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
